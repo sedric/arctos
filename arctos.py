bd62491428a541778893499a0e42910dbe1e25a9
sedric <sedric+github@sedric.fr>
2014-08-08 12:46:37 +0200

    Threads, style

--- arctos.py
+++ arctos.py
@@ -2,34 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import BaseHTTPServer, cgi
+from SocketServer import ThreadingMixIn
 import string
+import time
 
+# hashtable containing open channels and datas
 alloc = {}
 
 class Request_Handler(BaseHTTPServer.BaseHTTPRequestHandler):
   global alloc
+  protocol_version = 'HTTP/1.1'
 
-  def get_content(self):
-    index = self.path.translate(string.maketrans("/","z"))
-    self.wfile.write(alloc[index])
+  def data_present_in_channel(self,channel):
+    if channel in alloc:
+      return True
+    else:
+      return False
 
   def do_GET(self):
-# Should work, but it seems that we need to have a threaded python program
-#    while True:
-#      try:
-#        self.get_content()
-#      except KeyError:
-#        time.sleep(1)
-#
-# So let's continue with this for now
-    try:
-      self.get_content()
-    except KeyError:
-      self.send_response(404)
-      self.end_headers()
+    channel = self.path.translate(string.maketrans("/","z"))
+    while not self.data_present_in_channel(channel):
+      time.sleep(1)
+    self.send_response(200)
+    self.send_header('Content-Length ', len(alloc[channel]))
+    self.send_header('Connection', 'close')
+    self.end_headers()
+    self.wfile.write(alloc[channel])
 
   def do_POST(self):
-    index = self.path.translate(string.maketrans("/","z"))
+    channel = self.path.translate(string.maketrans("/","z"))
     ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
     if ctype == 'multipart/form-data':
       postvars = cgi.parse_multipart(self.rfile, pdict)
@@ -39,19 +40,27 @@ class Request_Handler(BaseHTTPServer.BaseHTTPRequestHandler):
     else:
       postvars = {}
     self.send_response(200)
+    self.send_header('Connection', 'close')
     self.end_headers()
-    alloc[index] = postvars
+    alloc[channel] = postvars
+
+class ThreadedHTTPServer(ThreadingMixIn, BaseHTTPServer.HTTPServer):
+      """Handle requests in a separate thread."""
 
 def httpd(i, p):
-  server = BaseHTTPServer.HTTPServer((i, p), Request_Handler)
+  server = ThreadedHTTPServer((i, p), Request_Handler)
   server.serve_forever()
 
 def main():
   import argparse
 
   parser = argparse.ArgumentParser(description='Messaging HTTP server')
-  parser.add_argument('-i', action='store', default="0.0.0.0", help='Interface used (default : all)')
-  parser.add_argument('-p', action='store', type=int, default='8080', help='Port used for listen (default : 8080)')
+  parser.add_argument('-i', action='store',
+                      default="0.0.0.0",
+                      help='Interface used (default : all)')
+  parser.add_argument('-p', action='store',
+                      type=int, default='8080',
+                      help='Port used for listen (default : 8080)')
   args = parser.parse_args()
   args = dict(args._get_kwargs())
 
